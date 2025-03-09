ff288130f5d91e87b952e7db3e9f8240b55f73c7
ParcivalDev <ParcivalDev@users.noreply.github.com>
2025-03-09 11:47:27 +0100

    Primeros pasos

--- Python_Backend/FastAPI/main.py
+++ Python_Backend/FastAPI/main.py
@@ -34,6 +34,26 @@ async def leer_item(item_id):
 
 # Si ponemos un valor que no sea un entero saltará un error
 # Ej: "hola", 6.8
-@app.get("/itemInt/{item_id}") 
+@app.get("/itemInt/{item_id}")
 async def leer_item(item_id: int):
     return {"item_id": item_id}
+
+
+# La primera siempre será utilizada ya que el path coincide primero. Esta segunda nunca se llega a mostrar
+@app.get("/itemInt/{item_id}")
+async def leer_item(item_id: int):
+    return {"item_id 2": item_id}
+
+
+# http://127.0.0.1:8000/items/?skip=0&limit=2
+# Resultado: [ {"item_uno": "Uno"}, {"item_dos": "Dos"}]
+# Devuelve un slice de la lista. Ee 0 a 10 devuelve los primeros 10
+# Lista de diccionarios: [{}, {}, {}]
+# Al añadir otro elemento al primer diccionario devuelve esos dos elementos
+items_db = [{"item_uno": "Uno", "ola": "adios"},
+            {"item_dos": "Dos"}, {"item_Tres": "Tres"}]
+@app.get("/items/")
+async def buscar_item(skip: int = 0, limit: int = 10):
+    return items_db[skip: skip + limit]
+
+
--- Python_Backend/FastAPI/usuarios.py
+++ Python_Backend/FastAPI/usuarios.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Iniciar el server: fastapi dev usuarios.py
 
+# Iniciar el server: fastapi dev usuarios.py
+# 127.0.0.1:8000/usuarios
 @app.get("/usuarios")
 async def usuarios():
     return "Hola usuario"
+
+
+
