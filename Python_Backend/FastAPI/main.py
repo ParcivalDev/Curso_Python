11ef3247e3cb4d081cb21fb14122209090e843c6
ParcivalDev <ParcivalDev@users.noreply.github.com>
2025-03-09 12:40:53 +0100

    Request Body - POST

--- Python_Backend/FastAPI/main.py
+++ Python_Backend/FastAPI/main.py
@@ -12,6 +12,7 @@ PUT: para actualizar datos.
 DELETE: para eliminar datos. """
 
 
+from pydantic import BaseModel
 from fastapi import FastAPI
 app = FastAPI()  # crea instancia de FastAPI
 
@@ -45,6 +46,7 @@ async def leer_item(item_id: int):
     return {"item_id 2": item_id}
 
 
+# Parámetros de Query
 # http://127.0.0.1:8000/items/?skip=0&limit=2
 # Resultado: [ {"item_uno": "Uno"}, {"item_dos": "Dos"}]
 # Devuelve un slice de la lista. Ee 0 a 10 devuelve los primeros 10
@@ -52,8 +54,41 @@ async def leer_item(item_id: int):
 # Al añadir otro elemento al primer diccionario devuelve esos dos elementos
 items_db = [{"item_uno": "Uno", "ola": "adios"},
             {"item_dos": "Dos"}, {"item_Tres": "Tres"}]
+
+
 @app.get("/items/")
 async def buscar_item(skip: int = 0, limit: int = 10):
     return items_db[skip: skip + limit]
 
 
+# ------------
+# Request Body
+# ------------
+# Cuando necesitas enviar datos desde un cliente (digamos, un navegador) a tu API, los envías como un request body.
+# Un request body es un dato enviado por el cliente a tu API. Un response body es el dato que tu API envía al cliente.
+
+# Necesitamos crear nuestro modelo de datos heredando de BaseModel
+# Con None marcamos opcionales. El resto son obligatorios
+class Producto(BaseModel):
+    nombre: str
+    descripcion: str | None = None
+    precio: float
+
+
+@app.post("/productos/")
+async def crear_producto(producto: Producto):
+    producto.nombre = producto.nombre.upper()
+    return producto
+
+# Seleccionamos POST con la url: http://127.0.0.1:8000/productos
+# En body escribimos:
+# {
+#   "nombre": "Casco",
+#   "precio": 324.56
+# }
+# y nos devuelve:
+#     {
+#   "nombre": "CASCO",
+#   "descripcion": null,
+#   "precio": 324.56
+# }
