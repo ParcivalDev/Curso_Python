71e124ed37296b9b10591e72ac21af8973ab21a8
ParcivalDev <ParcivalDev@users.noreply.github.com>
2025-04-12 00:00:00 +0200

    Herencia

--- IBM/Backend/Repaso/herencia.py
+++ IBM/Backend/Repaso/herencia.py
@@ -3,20 +3,26 @@
 class Animal:
     def comer(self):
         print("Comiendo")
-    
+
     def dormir(self):
         print("ZzZzZzZz")
-    
-    
+
+    def __str__(self):
+        return f"Animal: {super.__str__(self)}"
 
 
 class Perro(Animal):
     def hacer_sonido(self):
         print("Guau guau")
-        
-        
+
+    def dormir(self):
+        print("Perro: ZzZzZzZzZ")
+
+
 perro = Perro()
 
 perro.comer()
-perro.dormir()
+perro.dormir()  # Perro: ZzZzZzZzZ
 perro.hacer_sonido()
+
+print(perro.__str__())
